7376b27fc558deca7b45c05b4c8194c3bb240e99
Huy TQ <imhuytq@gmail.com>
2019-09-30 11:25:56 +0700

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+declare const gapi: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'youtube';
+export class AppComponent implements OnInit {
+  initialized = false;
+  user: any;
+  isSignedIn = false;
+  userName: string;
+  channelId = '';
+  items = [];
+  isLoading = false;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    gapi.load('client', () => {
+      gapi.client.init({
+        // apiKey: 'AIzaSyBqB01j128dFbzveCsLH5Hf2f6Yyqu-DMA',
+        clientId: '939309988257-nffoo68sk5s9b1dosn8skeodrpsolnl6.apps.googleusercontent.com',
+        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+        scope: 'https://www.googleapis.com/auth/youtube'
+      }).then(() => {
+        this.ngZone.run(() => {
+          this.initialized = true;
+          this.isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        });
+      });
+    });
+  }
+
+  signIn() {
+    gapi.auth2.getAuthInstance().signIn({
+      ux_mode: 'popup'
+    }).then((user: any) => {
+      this.ngZone.run(() => {
+        this.user = user;
+        this.userName = user.getBasicProfile().getName();
+      });
+    });
+  }
+
+  async get() {
+    this.isLoading = true;
+    const listChannelsResponse = await gapi.client.youtube.channels.list({
+      part: 'contentDetails',
+      id: this.channelId
+    });
+    const playlist = listChannelsResponse.result.items[0].contentDetails.relatedPlaylists.uploads;
+    let listPlaylistItemsResponse, nextPageToken;
+    let items = [];
+
+    do {
+      listPlaylistItemsResponse = await gapi.client.youtube.playlistItems.list({
+        part: 'snippet',
+        playlistId: playlist,
+        maxResults: 50,
+        pageToken: nextPageToken
+      });
+      nextPageToken = listPlaylistItemsResponse.result.nextPageToken;
+      const videosIds = [];
+    
+      listPlaylistItemsResponse.result.items.forEach(video => {
+        videosIds.push(video.snippet.resourceId.videoId);
+      });
+
+      if (videosIds.length > 0) {
+        const listVideosResponse = await gapi.client.youtube.videos.list({
+          part: 'contentDetails,id,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails',
+          id: videosIds.join(',')
+        });
+        this.ngZone.run(() => {
+          items.push(...listVideosResponse.result.items);
+        });
+      }
+    } while (nextPageToken);
+
+    this.items = items;
+    this.isLoading = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgZorroAntdModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
